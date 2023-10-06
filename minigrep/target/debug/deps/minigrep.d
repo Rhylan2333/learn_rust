8f3e29f1546a4fba65efa082134ea8bd9b19dc88
Rhylan Cai <caucaicai@qq.com>
2023-10-06 14:09:10 +0800

    minigrep

---
+++ minigrep/target/debug/deps/minigrep.d
@@ -0,0 +1,5 @@
+C:\Users\cauca\Desktop\learn_rust\minigrep\target\debug\deps\minigrep.exe: src\main.rs
+
+C:\Users\cauca\Desktop\learn_rust\minigrep\target\debug\deps\minigrep.d: src\main.rs
+
+src\main.rs:
---
+++ minigrep/target/debug/minigrep.d
@@ -0,0 +1,1 @@
+C:\Users\cauca\Desktop\learn_rust\minigrep\target\debug\minigrep.exe: C:\Users\cauca\Desktop\learn_rust\minigrep\src\main.rs
